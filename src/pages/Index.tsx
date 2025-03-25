b7adf6d5b35dec1029080545d0fe872bc736751f
catherinehoang44 <59583462+catherinehoang44@users.noreply.github.com>
2025-03-25 15:50:27 -0400

    text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -93,7 +93,7 @@ const Index = () => {
           <div className="w-full max-w-4xl">
             <h2 className="text-xl font-medium mb-5">Objective</h2>
             <p className="text-gray-600 dark:text-gray-300 mb-8 leading-relaxed">
-              This document outlines the UX (User Experience) business requirements for the Adobe Certification Portal (ACP). 
+              This document outlines the user experience (UX) business requirements for the Adobe Certification Portal (ACP). 
               The ACP serves as the primary platform for users to discover, register for, complete, and manage their Adobe DX product certifications.
             </p>
             {isLoading ? (
